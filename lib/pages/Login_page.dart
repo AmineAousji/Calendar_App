0ab02148ce2bda81b71aa95c6962dbbc18792312
Emene-Pesh <emeneche@gmail.com>
2022-12-10 10:28:38 +0100

    Sign in Page

--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -56,6 +56,12 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 TextButton(
                   onPressed: () {
                     //forgot password screen
+
+                    Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder:(context) => SignUp(),
+                        )
+                    );
+                    //signup screen
                   },
                   child: const Text(
                     'Forgot Password',
@@ -96,7 +102,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                       ),
                       onPressed: () {
                         Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => MainPage(),
+                          builder: (context) => SignUp(),
                         ));
                         //signup screen
                       },
@@ -107,4 +113,81 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               ],
             )));
   }
+}
+
+class SignUp extends StatelessWidget {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  // Provider.of<EventProvider>(context, listen: false).syncEventsFromDB();
+
+  @override
+  Widget build(BuildContext context) =>
+      Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+          centerTitle: true,
+        ),
+        body:
+        ListView(
+              children: <Widget>[
+          Container(
+          alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Sign up',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              )),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'email',
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: TextField(
+              obscureText: true,
+              controller: passwordController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                child: const Text('Login'),
+                onPressed: () async {
+                  final message = await AuthService().registration(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+                  if (message!.contains('Success')) {
+                    Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder:(context) => MainPage(),
+                        )
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar( const SnackBar(
+                      content: Text("Loged in"),
+                    ));
+                  }
+                  else {
+                    ScaffoldMessenger.of(context).showSnackBar( const SnackBar(
+                      content: Text("Please enter correct information "),
+                    ));
+                  }
+                },
+              )
+          ),
+        ])
+  );
 }
